dbbbcdc4ee59104b785b56126f69ab1f5c0b1ea0
Fahad1785 <syedfahad1785@gmail.com>
2022-01-16 17:15:58 +0530

    python codes

---
+++ cocatanation.py
@@ -0,0 +1,7 @@
+a = "Fahad" 
+b = "syed"
+Name = (a+b)
+print (Name)
+
+a = input("enter any number:  ")
+print(a)
---
+++ string.py
@@ -0,0 +1,14 @@
+# Program to calculate average of two numbers
+
+print("Hellow world")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+Answer = ((float(a)+float(b))/2)
+print (int(Answer))
+
+print("Answer")
+
+#this program will calculate the average of two numbers
+
+
