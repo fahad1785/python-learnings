e8c4cc16ab124589efc27ebf5f4059d597d024b5
Fahad1785 <syedfahad1785@gmail.com>
2022-01-31 21:20:05 +0530

    learning python

---
+++ variablecase.py
@@ -0,0 +1,16 @@
+#Multi Words Variable Names
+
+#Camel Case
+#WEach word, except the first, starts with a capital letter
+myVariableName = "John"
+print(myVariableName)
+
+#Pascal Case
+#Each word starts with a capital letter
+MyVariableName = "John"
+print(MyVariableName)
+
+#Snake Case
+#Each word is separated by an underscore character:
+my_variable_name = "John"
+print(my_variable_name)
