59cfcdc1014603df617d01f298cb8904f1c836c4
Fahad1785 <syedfahad1785@gmail.com>
2022-02-01 15:49:03 +0530

    python variable conversion

---
+++ vartypes.py
@@ -0,0 +1,13 @@
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Convert from one type to another
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
