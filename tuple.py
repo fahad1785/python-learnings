eda4ed8689cb73692c635c0646357c66ea7d2f10
Fahad1785 <syedfahad1785@gmail.com>
2022-02-04 22:43:28 +0530

    what's tuple

---
+++ tuple.py
@@ -0,0 +1,10 @@
+#A tuple is a collection of objects which ordered and immutable.
+#Tuples are sequences, just like lists.
+#The differences between tuples and lists are, the tuples cannot be changed unlike,
+#lists and tuples use parentheses, whereas lists use square brackets.
+
+tup1 = ('physics', 'chemistry', 1997, 2000)
+tup2 = (1, 2, 3, 4, 5 )
+tup3 = "a", "b", "c", "d"
+
+print (tup1)
